ee86eb75d268a81b72239953f1294ce517c7a3bb
Vitor Oliveira <vmoliveira91@gmail.com>
2021-08-17 13:05:13 -0300

    Correção do exercício 04

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -4,3 +4,11 @@ document.querySelector('[data-js="app"]').innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+
+const link = document.querySelector('[data-js="link"]')
+
+link.addEventListener("click", (event) => {
+  event.preventDefault()
+  const element = document.querySelector('[data-js="app"]')
+  element.hidden = !element.hidden
+}, false)
