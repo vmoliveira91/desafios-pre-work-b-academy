c7b0981447ff5fd59c2481ec05928478f7e07fe0
Vitor Oliveira <vmoliveira91@gmail.com>
2021-08-20 12:47:51 -0300

    Correção do exercício 02

---
+++ dia4/app/cars.js
@@ -0,0 +1,34 @@
+async function getCars(url) {
+    const response = await fetch(url, {
+        method: 'GET',
+        //mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+    return response
+}
+
+const cars = await getCars('http://localhost:3333/cars')
+    .then((result) => result.ok && result.json())
+    .then((result) => result)
+
+const propriedades = ['image', 'brandModel', 'year', 'plate', 'color']
+
+const tbody = document.querySelector('[data-js="table-body"]')
+
+if(cars.length === 0) {
+    const tr = document.createElement('tr')
+    tr.textContent = 'Nenhum carro encontrado'
+    tbody.appendChild(tr)
+} else {
+    cars.forEach((car) => {
+        const tr = document.createElement('tr')
+        propriedades.forEach((propriedade) => {
+            const td = document.createElement('td')
+            td.textContent = car[propriedade]
+            tr.appendChild(td)
+        })
+        tbody.appendChild(tr)
+    })
+}
