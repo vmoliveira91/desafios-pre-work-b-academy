6e80c9d9cc29948b328925c5b8f94ea5855805cf
Vitor Oliveira <vmoliveira91@gmail.com>
2021-08-21 16:27:11 -0300

    Refatoração no código do dia 4

--- dia4/app/cars.js
+++ dia4/app/cars.js
@@ -1,64 +1,90 @@
 const url = 'http://localhost:3333/cars'
-
-const cars = await fetch(url, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-    })
-    .then((result) => result.json())
-
 const propriedades = ['image', 'brandModel', 'year', 'plate', 'color']
-
 const tbody = document.querySelector('[data-js="table-body"]')
+const form = document.querySelector('[data-js="form-cars"]')
 
-if(cars.length === 0) {
-    const tr = document.createElement('tr')
-    tr.textContent = 'Nenhum carro encontrado'
-    tr.classList.add('empty')
-    tbody.appendChild(tr)
-} else {
-    cars.forEach((car) => {
-        const tr = document.createElement('tr')
-
-        propriedades.forEach((propriedade) => {
-            const td = document.createElement('td')
-            td.textContent = car[propriedade]
-            tr.appendChild(td)
-        })
-
-        const field = document.createElement('td')
-        const button = document.createElement('button')
-        
-        button.value = car['plate']
-        button.textContent = 'Deletar'
-        button.classList.add('delete')
-        
-        field.appendChild(button)
-        tr.appendChild(field)
-        tbody.appendChild(tr)
-    })
+const getCars = (url) => {
+    const cars = fetch(url)
+        .then((result) => result.json())
+    
+    return cars
 }
 
-const form = document.querySelector('[data-js="form-cars"]')
+const addButtonListener = (button) => {
+    button.addEventListener('click', async (e) => {
+        e.preventDefault
+
+        const plate = e.target.value
+        
+        const response = await fetch(url, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                plate: plate,
+            })
+        })
+        .then((result) => result.json())
+
+        if(response.message === `O carro com placa ${plate} foi removido com sucesso`) {
+            const row = e.target.parentElement.parentElement
+            row.innerHTML = ''
+        }
+
+        const cars = await getCars(url)
+        
+        if(cars.length === 0) {
+            const tr = document.createElement('tr')
+            tr.textContent = 'Nenhum carro encontrado'
+            tr.classList.add('empty')
+            tbody.appendChild(tr)
+        }        
+    })    
+}
+
+const fillRow = (car) => {
+    const tr = document.createElement('tr')
+
+    propriedades.forEach((propriedade) => {
+        const td = document.createElement('td')
+        td.textContent = car[propriedade]
+        tr.appendChild(td)
+    })
+
+    const td = document.createElement('td')
+    const button = document.createElement('button')
+    
+    button.value = car['plate']
+    button.textContent = 'Deletar'
+    button.classList.add('delete')
+    addButtonListener(button)
+
+    td.appendChild(button)
+    tr.appendChild(td)
+
+    return tr
+}
 
 form.addEventListener('submit', async (e) => {
     e.preventDefault()
 
     const elements = [...e.target.elements]
 
+    const car = {
+        image: elements[0].value,
+        brandModel: elements[1].value,
+        year: elements[2].value,
+        plate: elements[3].value,
+        color: elements[4].value,
+    }
+
     const response = await fetch(url, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({
-            image: elements[0].value,
-            brandModel: elements[1].value,
-            year: elements[2].value,
-            plate: elements[3].value,
-            color: elements[4].value,
-        })
+        body: JSON.stringify(car)
     })
     .then((result) => result.json())
 
@@ -81,67 +107,7 @@ form.addEventListener('submit', async (e) => {
         if(error !== null)
             error.innerHTML = ''
         
-        const table = document.querySelector('[data-js="table-body"]')
-
-        const row = document.createElement('tr')
-            
-        elements.forEach((elemento) => {
-            if(elemento.value !== '') {
-                const field = document.createElement('td')    
-                field.textContent = elemento.value
-                row.appendChild(field)
-            }        
-        })
-
-        const field = document.createElement('td')
-        const button = document.createElement('button')
-
-        button.value = elements[3].value
-        button.textContent = 'Deletar'
-        button.classList.add('delete')
-
-        button.addEventListener('click', async (e) => {
-            e.preventDefault
-
-            const plate = e.target.value
-            
-            const response = await fetch(url, {
-                method: 'DELETE',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    plate: plate,
-                })
-            })
-            .then((result) => result.json())
-
-            if(response.message === `O carro com placa ${plate} foi removido com sucesso`) {
-                const row = e.target.parentElement.parentElement
-                row.innerHTML = ''
-            }
-
-            const cars = await fetch(url, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-            })
-            .then((result) => result.json())
-        
-            const tbody = document.querySelector('[data-js="table-body"]')
-            
-            if(cars.length === 0) {
-                const tr = document.createElement('tr')
-                tr.textContent = 'Nenhum carro encontrado'
-                tr.classList.add('empty')
-                tbody.appendChild(tr)
-            }
-        })
-        
-        field.appendChild(button)
-        row.appendChild(field)
-        table.appendChild(row)
+        tbody.appendChild(fillRow(car))
         
         e.target.reset()
         
@@ -150,45 +116,15 @@ form.addEventListener('submit', async (e) => {
     }    
 })
 
-const buttons = [...document.querySelectorAll('button.delete')]
+const cars = await getCars(url)
 
-buttons.forEach((button) => {
-    button.addEventListener('click', async (e) => {
-        e.preventDefault
-
-        const plate = e.target.value
-        
-        const response = await fetch(url, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                plate: plate,
-            })
-        })
-        .then((result) => result.json())
-
-        if(response.message === `O carro com placa ${plate} foi removido com sucesso`) {
-            const row = e.target.parentElement.parentElement
-            row.innerHTML = ''
-        }
-
-        const cars = await fetch(url, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        })
-        .then((result) => result.json())
-    
-        const tbody = document.querySelector('[data-js="table-body"]')
-        
-        if(cars.length === 0) {
-            const tr = document.createElement('tr')
-            tr.textContent = 'Nenhum carro encontrado'
-            tr.classList.add('empty')
-            tbody.appendChild(tr)
-        }
+if(cars.length === 0) {
+    const tr = document.createElement('tr')
+    tr.textContent = 'Nenhum carro encontrado'
+    tr.classList.add('empty')
+    tbody.appendChild(tr)
+} else {
+    cars.forEach((car) => {
+        tbody.appendChild(fillRow(car))
     })
-})
+}
